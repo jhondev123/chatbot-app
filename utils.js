941fd0fce25ef10387b8705d11a5eb29ac539ec5
jhondev123 <jhonattan.curtarelli@outlook.com>
2024-11-09 11:28:09 -0300

    refactor: separando algumas funcionalidades para o arquivo utils.js

---
+++ utils.js
@@ -0,0 +1,29 @@
+function verifyMessageIsValidContact(msg){
+    return msg.from.endsWith('@c.us')
+
+}
+
+function verifyMessageIsValidGroup(msg){
+    return msg.from.endsWith('@g.us')
+}
+
+async function simulatingTyping (chat,delay){
+    await delay(3000);
+    await chat.sendStateTyping();
+    await delay(3000);
+}
+
+function getContactData(msg){
+    return msg.getContact();
+}
+
+function verifyKeyWordIsValid(msg){
+    return msg.body.match(/(menu|Menu|dia|tarde|noite|oi|Oi|Olá|olá|ola|Ola)/i);
+}
+
+module.exports = {
+    verifyMessageIsValidContact,
+    verifyMessageIsValidGroup,
+    verifyKeyWordIsValid,
+    simulatingTyping
+};
